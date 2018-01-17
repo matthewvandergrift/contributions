ee6427b01e12730282aba768ae1ae6b18c8d85a1
Vandy <matthew.vandergrift@live.com>
2018-01-17 14:21:29 -0500

    Added InOrder and PostOrder unit test for BTree regression traversal

--- Matthew.Vandergrift.Technical.Screening.Tests/Areas/Regression/BTreeTests.cs
+++ Matthew.Vandergrift.Technical.Screening.Tests/Areas/Regression/BTreeTests.cs
@@ -12,14 +12,15 @@ namespace Matthew.Vandergrift.Technical.Screening.Tests.Areas.Regression
             var bTree = new BTree();
             bTree.Insert(20);
             bTree.Insert(15);
-            bTree.Insert(13);
             bTree.Insert(16);
+            bTree.Insert(13);
             bTree.Insert(25);
             bTree.Insert(22);
             bTree.Insert(45);
 
             return bTree;
         }
+
         [TestMethod]
         public void BTreeTraversalPreOrder()
         {
@@ -29,6 +30,36 @@ namespace Matthew.Vandergrift.Technical.Screening.Tests.Areas.Regression
 
             //Act
             tree.PreOrder(tree.ReturnRoot());
+            var result = tree.output.ToString();
+
+            //Assert
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestMethod]
+        public void BTreeTraversalInOrder()
+        {
+            //Arrange
+            var tree = CreateTree();
+            var expected = "13 15 16 20 22 25 45 ";
+
+            //Act
+            tree.InOrder(tree.ReturnRoot());
+            var result = tree.output.ToString();
+
+            //Assert
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestMethod]
+        public void BTreeTraversalPostOrder()
+        {
+            //Arrange
+            var tree = CreateTree();
+            var expected = "13 16 15 22 45 25 20 ";
+
+            //Act
+            tree.PostOrder(tree.ReturnRoot());
             var result = tree.output.ToString();
 
             //Assert

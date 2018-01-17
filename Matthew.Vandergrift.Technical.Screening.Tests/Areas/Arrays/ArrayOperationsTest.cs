681d63e984dd5d9b47598320f3c03b659f071e11
Vandy <matthew.vandergrift@live.com>
2018-01-17 12:41:21 -0500

    remove redundant type specification

--- Matthew.Vandergrift.Technical.Screening.Tests/Areas/Arrays/ArrayOperationsTest.cs
+++ Matthew.Vandergrift.Technical.Screening.Tests/Areas/Arrays/ArrayOperationsTest.cs
@@ -14,13 +14,13 @@ namespace Matthew.Vandergrift.Technical.Screening.Tests.Areas.Arrays
         {
             //Arrange
             var arrayOperations = new ArrayOperations();
-            var arrayInput = new int[][]
+            var arrayInput = new []
             {
-                new int[] {1, 2, 3, 5, 4},
-                new int[] {1, 3, 2, 5, 4},
-                new int[] {1, 4, 3, 5, 2},
-                new int[] {1, 5, 3, 2, 4},
-                new int[] {2, 1, 3, 5, 4},
+                new [] {1, 2, 3, 5, 4},
+                new [] {1, 3, 2, 5, 4},
+                new [] {1, 4, 3, 5, 2},
+                new [] {1, 5, 3, 2, 4},
+                new [] {2, 1, 3, 5, 4},
             };
             var expected = new HashSet<int> { 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5 };
             

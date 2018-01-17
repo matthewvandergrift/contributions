2f5e602d7c893cadcb0c811faa7b8faa067484fb
Vandy <matthew.vandergrift@live.com>
2018-01-16 22:10:45 -0500

    Added  Regression Tests with a simple yet effective fibonacci calc.

---
+++ Matthew.Vandergrift.Technical.Screening.Tests/Areas/Regression/RegressionTests.cs
@@ -0,0 +1,22 @@
+﻿using Matthew.Vandergrift.Technical.Screening.Areas.Regression;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matthew.Vandergrift.Technical.Screening.Tests.Areas.Regression
+{
+    [TestClass]
+    public class RegressionTests
+    {
+        [TestMethod]
+        public void FibonacciTo5Places()
+        {
+            //Arrange
+            var regression = new RegressionQuick();
+
+            //Act
+            var result = regression.CalculateFibonacci(6);
+
+            //Assert
+            Assert.IsTrue(result == 5);
+        }
+    }
+}
---
+++ Matthew.Vandergrift.Technical.Screening/Areas/Regression/RegressionQuick.cs
@@ -0,0 +1,17 @@
+﻿namespace Matthew.Vandergrift.Technical.Screening.Areas.Regression
+{
+    public class RegressionQuick
+    {
+        public long CalculateFibonacci(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                case 1:
+                    return number;
+                default:
+                    return CalculateFibonacci(number - 2) + CalculateFibonacci(number - 1);
+            }
+        }
+    }
+}

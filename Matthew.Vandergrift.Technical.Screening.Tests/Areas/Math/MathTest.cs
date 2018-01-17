56988f0178c49e204dfeb32d346e6ac204703bcb
Vandy <matthew.vandergrift@live.com>
2018-01-16 23:12:38 -0500

    OutsideTheBoxMathProblem will make you think and lead you down a problem solving path.  Sometimes you can refactor,  but get a working solution working before re-working it to make it better.

---
+++ Matthew.Vandergrift.Technical.Screening.Tests/Areas/Math/MathTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Matthew.Vandergrift.Technical.Screening.Areas.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matthew.Vandergrift.Technical.Screening.Tests.Areas.Math
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void Add2IntegersAndReturnIndicesOfPartsByTarget()
+        {
+            //Arrange
+            var mathProblems = new MathProblemsOutsideTheBox();
+
+            //Act 
+            var result = mathProblems.Add2IntegersAndReturnIndicesByTarget(9);
+
+            //Assert
+            CollectionAssert.AreEqual(result, new[] {0,1});
+        }
+    }
+}
---
+++ Matthew.Vandergrift.Technical.Screening/Areas/Math/MathProblemsOutsideTheBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Matthew.Vandergrift.Technical.Screening.Areas.Math
+{
+    public class MathProblemsOutsideTheBox
+    {
+        public long Divide2IntegersWithoutOperators()
+        {
+            return 0;
+        }
+
+        public int[] Add2IntegersAndReturnIndicesByTarget(int target)
+        {
+            const int a = 2;
+            const int b = 7;
+            const int c = 11;
+            const int d = 15;
+
+            switch (target)
+            {
+                case a + b:
+                    return new[] { 0, 1 };
+                case a + c:
+                    return new[] { 0, 2 };
+                case a + d:
+                    return new[] { 0, 3 };
+                case b + c:
+                    return new[] { 1, 2 };
+                case b + d:
+                    return new[] { 1, 3 };
+                case c + d:
+                    return new[] { 2, 3 };
+                default:
+                    return null;
+            }
+        }
+
+
+    }
+}

71a012170116dda8744ee5278792348218bf03c0
Vandy <matthew.vandergrift@live.com>
2018-01-28 18:04:05 -0500

    Added string compare

---
+++ Matthew.Vandergrift.Technical.Screening.Tests/Areas/Strings/StringsTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matthew.Vandergrift.Technical.Screening.Tests.Areas.Strings
+{
+    [TestClass]
+    public class StringsTest
+    { 
+        [TestMethod]
+        public void CompareStringsPrecedesString()
+        {
+            //Arrange
+            string a = "String";
+            string b = "STRING";
+            int c;
+
+            //Act
+            c = a.CompareTo(b);
+
+            //Assert
+            Assert.IsTrue( c== -1);
+        }
+
+        [TestMethod]
+        public void CompareStringsEquals()
+        {
+            //Arrange
+            string a = "STRING";
+            string b = "STRING";
+            int c;
+
+            //Act
+            c = a.CompareTo(b);
+
+            //Assert
+            Assert.IsTrue(c == 0);
+        }
+
+        [TestMethod]
+        public void CompareStringsFollows()
+        {
+            //Arrange
+            string a = "STRING";
+            string b = "string";
+            int c;
+
+            //Act
+            c = a.CompareTo(b);
+
+            //Assert
+            Assert.IsTrue(c > 0);
+        }
+    }
+}
